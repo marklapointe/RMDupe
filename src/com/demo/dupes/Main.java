817db56f59dd6956b05a77423d03ab991a110635
Mark LaPointe <mark@revynet.org>
2020-07-11 21:18:52 -0400

    Added implementation(s)

--- src/com/demo/dupes/Main.java
+++ src/com/demo/dupes/Main.java
@@ -3,6 +3,11 @@
  */
 package com.demo.dupes;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * @author marklapointe
  *
@@ -13,7 +18,7 @@ public class Main {
 	 * 
 	 */
 	public Main() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	/**
@@ -21,6 +26,27 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Start");
+		Integer[] data = new Integer[] {1, 1, 2, 2, 3, 4, 4, 4, 4};
+		
+		// The easy way
+		Set<Integer> result = new TreeSet<Integer>();
+		for(Integer c: data) {
+			result.add(c);
+		}
+		System.out.println(result);
+		
+		// The hard way
+		Integer lastValue = null;
+		List<Integer> result2 = new ArrayList<Integer>();
+		for(Integer c: data) {
+			if (lastValue != c) {
+				result2.add(c);
+				lastValue = c;
+			}
+		}
+		System.out.println(result2);
+		
+		System.out.println("End");
 	}
 
 }
