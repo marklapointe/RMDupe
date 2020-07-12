1825fd13b28f828e11bb4289dbbecd8a9a02e2c6
Mark LaPointe <mark@revynet.org>
2020-07-11 21:43:05 -0400

    More ways to skin a cat, added pure array implementation

--- src/com/demo/dupes/Main.java
+++ src/com/demo/dupes/Main.java
@@ -37,14 +37,44 @@ public class Main {
 		
 		// The hard way
 		Integer lastValue = null;
+		int removeCount = 0;
 		List<Integer> result2 = new ArrayList<Integer>();
 		for(Integer c: data) {
 			if (lastValue != c) {
 				result2.add(c);
 				lastValue = c;
+			} else {
+				removeCount++;
 			}
 		}
+		Integer[] resultarr = new Integer[result2.size()];
+		for (int i = 0; i < result2.size();i++) {
+			resultarr[i] = result2.get(i);
+		}
+		System.out.println(removeCount);
 		System.out.println(result2);
+		for(Integer i: resultarr) System.out.println(i);
+		System.out.println();
+		// The harder way
+		Integer lastValueHarder = null;
+		int removeCountHarder = 0;
+		int addCounter = 0;
+		int[] retdata = new int[data.length];
+		for(Integer c: data) {
+			if (lastValue != c) {
+				lastValue = c;
+				retdata[addCounter] = c;
+				addCounter++;
+			} else {
+				removeCount++;
+			}
+		}
+		int[] newdata = new int[addCounter];
+		System.arraycopy(retdata, 0, newdata, 0, addCounter);
+		for(Integer i: newdata) System.out.println(i);
+
+
+		System.out.println();
 		
 		System.out.println("End");
 	}
